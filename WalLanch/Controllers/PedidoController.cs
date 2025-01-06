4a9c9eafcce8969d3ede20a03d2cf70983ef072b
Wallace Cabral <wallacecabral03@gmail.com>
2025-01-06 17:15:08 -0300

    criando controlador PedidoController

---
+++ WalLanch/Controllers/PedidoController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WalLanches.Models;
+using WalLanches.Repositories;
+using WalLanches.Repositories.Interfaces;
+
+namespace WalLanches.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private readonly IPedidoRepository _pedidoRepository;
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public PedidoController(IPedidoRepository pedidoRepository, CarrinhoCompra carrinhoCompra)
+        {
+            _pedidoRepository = pedidoRepository;
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        [HttpGet]
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Pedido pedido)
+        {
+            return View();
+        }
+    }
+}
