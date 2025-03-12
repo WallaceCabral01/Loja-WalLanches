3442439da2408a72b8bd1bb2ef6bcd01657037a0
Wallace Cabral <wallacecabral03@gmail.com>
2025-03-12 01:52:36 -0300

    Criando o controlador e os métodos actions

---
+++ WalLanch/Areas/Admin/Controllers/AdminGraficoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WalLanches.Areas.Admin.Controllers
+{
+    public class AdminGraficoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
